5371fee4089f2caebfa547ab7448c747299e18f9
Gerald Wodni <gerald.wodni@gmail.com>
2023-10-26 16:56:32 +0200

    keyboard input added, rendering blank canvas

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 /*
  * Squarefighter - initial implementation in C
+ * fighting squares with nothing but squares,
+ * say no to fance models!
+ *
  * using libSDL2 for graphics & input
  * using chipmunk-physics for physics :P
  *
@@ -19,6 +22,8 @@ typedef struct {
     int width;
     int height;
     char *title;
+    int running;
+    unsigned char *keys;
 } Context;
 
 void bailOut( char *message ) {
@@ -39,22 +44,34 @@ void initSDL( Context *context ) {
         bailOut( "Could not create Renderer." );
 
     context->surface = SDL_GetWindowSurface( context->window );
-    SDL_SetRenderDrawColor( context->renderer, 0x00, 0x80, 0x00, 0xFF );
 }
 
 void update( Context *context ) {
+    if( context->keys[ SDL_SCANCODE_ESCAPE ] )
+        context->running = 0;
+}
+
+void draw( Context *context ){
+    
 }
 
 void render( Context *context ) {
+    SDL_SetRenderDrawColor( context->renderer, 0x00, 0x80, 0x00, 0xFF );
+    SDL_RenderClear( context->renderer );
+
+    draw( context );
+
+    SDL_RenderPresent( context->renderer );
 }
 
 int main( int argc, char **argv ) {
     Context context;
+    context.running = -1;
 
     char *title = argv[0];
     if( strncmp( title, "./", 2 ) == 0 )
         title += 2;
-    if( title[0] > 'A' )
+    if( title[0] > 'Z' )
         title[0] -= 'a' - 'A';
 
     context.title = title;
@@ -64,7 +81,24 @@ int main( int argc, char **argv ) {
     printf("Pre-init" );
     initSDL( &context );
     printf("Post-init" );
-    SDL_Delay( 1000 );
+
+    while( context.running )  {
+        SDL_Event event;
+        while( SDL_PollEvent(&event) )
+            switch( event.type ) {
+                case SDL_QUIT:
+                    context.running = 0;
+                    break;
+
+                default:
+                    break;
+            }
+        context.keys = SDL_GetKeyboardState(NULL);
+
+        update( &context );
+        render( &context );
+    }
+
     printf("Done" );
     return 0;
 }
